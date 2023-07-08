e82b5234710ad706109562486ffa82c974733c66
Vorasits <vorasit.som@avlgb.com>
2023-07-09 04:13:56 +0700

    update navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,16 +1,16 @@
 'use client';
- 
+
 import { useState } from 'react';
- 
+
 
 export default function Navbar() {
 
-    const handleClick = (e:any) => {
-         setCount(!count);
-      };
+    const handleClick = (e: any) => {
+        setCount(!count);
+    };
 
     const [count, setCount] = useState(false);
-  
+
     return (
         <>
             <nav className="bg-gray-800">
@@ -33,15 +33,15 @@ export default function Navbar() {
                         <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                             <div className="flex flex-shrink-0 items-center">
                                 <img className="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"></img>
-                                <img className="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"></img>
+                                <img className="hidden h-8 w-auto lg:block" src="./logo.png" alt="Your Company"></img>
                             </div>
                             <div className="hidden sm:ml-6 sm:block">
                                 <div className="flex space-x-4">
 
-                                    <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-                                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
-                                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-                                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+                                    <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">รายการ</a>
+                                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">บทความ</a>
+                                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">ติดต่อ</a>
+
                                 </div>
                             </div>
                         </div>
@@ -56,7 +56,7 @@ export default function Navbar() {
 
                             <div className="relative ml-3">
                                 <div>
-                        
+
 
 
                                     <button type="button" onClick={() => handleClick(true)} className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
@@ -65,7 +65,7 @@ export default function Navbar() {
                                     </button>
                                 </div>
 
-                           
+
                                 <div className={`absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${count === true ? "" : "hidden"}`} role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
                                     <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</a>
                                     <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</a>
