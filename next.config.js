e417c9b89d02e843dcb3451660788dd691fa023c
Vorasits <vorasit.som@avlgb.com>
2024-01-10 01:26:16 +0700

    update new config for tailwind link & new image & navbar image good resolution

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'assets.example.com',
-          port: '',
-          pathname: '/account123/**',
-        },
-      ],
+      domains: ['tailwindui.com'],
     },
   }
 
 module.exports = nextConfig
+
